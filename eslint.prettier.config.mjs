c1681fca012cfaf2aaed9149726eae7cbc70c12f
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2024-12-13 13:17:47 +0545

    feat: add prettier config

---
+++ eslint.prettier.config.mjs
@@ -0,0 +1,22 @@
+import eslintConfigPrettier from 'eslint-config-prettier';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import tseslint from 'typescript-eslint';
+
+// eslint-config-prettier: Disables ESLint rules that conflict with Prettier.
+// eslint-plugin-prettier: Integrates Prettier directly into ESLint as a rule (prettier/prettier).
+
+export default tseslint.config({
+  extends: [
+    eslintConfigPrettier, // Turns off all rules that are unnecessary or might conflict with Prettier.
+    eslintPluginPrettierRecommended,
+  ],
+  rules: {
+    'prettier/prettier': [
+      'warn',
+      {},
+      {
+        usePrettierrc: true,
+      },
+    ], // Enforce Prettier formatting
+  },
+});
