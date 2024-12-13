c5c7c5875c99b83793f9deaca8e13972754f268f
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2024-12-13 11:56:22 +0545

    feat(storybook): update global styles and initialize MSW

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,4 +1,25 @@
 import type { Preview } from '@storybook/react';
 import { initialize, mswLoader } from 'msw-storybook-addon';
 
-import '../src/index.css';
+import '../src/app/globals.css';
+
+
+// Initialize MSW
+initialize();
+
+// replace with the name of your tailwind css file
+const preview: Preview = {
+    parameters: {
+      controls: {
+        matchers: {
+          color: /(background|color)$/i,
+          date: /Date$/i,
+        },
+      },
+    },
+    loaders: [mswLoader],
+    decorators: [],
+  };
+  
+  export default preview;
+  
