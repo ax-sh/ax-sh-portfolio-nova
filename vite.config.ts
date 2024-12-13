ff39a0b0c85f3fcb672422c77fa7d60f07e716d4
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2024-12-13 12:02:22 +0545

    feat: add initial Vite configuration file

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import react from '@vitejs/plugin-react';
+import autoprefixer from 'autoprefixer';
+import tailwindcss from 'tailwindcss';
+import { defineConfig } from 'vite';
+import { qrcode } from 'vite-plugin-qrcode';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+// https://vite.dev/config/
+export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [tailwindcss, autoprefixer],
+    },
+  },
+  plugins: [react(), qrcode(), tsconfigPaths()],
+  server: {
+    host: '0.0.0.0',
+  },
+});
