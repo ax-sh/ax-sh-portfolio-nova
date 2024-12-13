f0a0cb6cb9d00543da19aad76bb3bfe0e85df2e7
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2024-12-13 12:02:13 +0545

    chore: update dependencies and improve layout component

--- src/ui/index.tsx
+++ src/ui/index.tsx
@@ -1,5 +1,5 @@
 import type { PropsWithChildren } from "react";
 
-export default function Layout({ children }: PropsWithChildren) {
+export default function Layout({ children }: Readonly<PropsWithChildren>) {
 	return <main>{children}</main>;
 }
