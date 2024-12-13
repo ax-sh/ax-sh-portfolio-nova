81a149948e9fa764bbd95d46a0e2bf034afe5118
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2024-12-13 11:59:11 +0545

    feat(ui): add Layout component and storybook integration

---
+++ src/ui/index.stories.tsx
@@ -0,0 +1,25 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import Layout from "./index.tsx";
+
+// Define metadata for the component
+const meta: Meta<typeof Layout> = {
+	title: "Mandala/Lotus", // Title path in Storybook
+	component: Layout, // Link the component to the story
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Layout>;
+
+// Define a story with args
+export const Primary: Story = {
+	args: {
+		children: (
+			<div className={"bg-red-100"}>
+				<h1>Custom Title</h1>
+				<p>Custom content inside the layout component.</p>
+			</div>
+		),
+	},
+};
