20f66d4232456aaea71c51ab1d3632b70ddd5cc7
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2024-12-18 13:47:48 +0545

    chore: add typos configuration and update next config

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   output: 'export',
   reactStrictMode: true,
   devIndicators: { buildActivity: true, appIsrStatus: true },
+  images: { unoptimized: true },
 };
 
 export default nextConfig;
