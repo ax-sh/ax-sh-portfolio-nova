727cb0cda11049be91f09909b9aa907077ae7564
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2024-12-18 14:33:26 +0545

    feat: wip

--- src/app/page.test.tsx
+++ src/app/page.test.tsx
@@ -1,3 +1,7 @@
+import { render } from '@testing-library/react';
+
+import Home from './page';
+
 function sum(a: number, b: number) {
   return a + b;
 }
@@ -8,7 +12,8 @@ test('adds 1 + 2 to equal 3', async () => {
 
 describe('Home Profile Page', () => {
   it('Should return the home page', () => {
-    const e = 3;
-    expect(e).toBe(3);
+    const ele = render(<Home />);
+    expect(ele).toBeInTheDocument();
+    // expect(e).tobeInThe(3);
   });
 });
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,18 @@
 /// <reference types="vitest" />
+import react from '@vitejs/plugin-react';
+import path from 'node:path';
+import tsconfigPaths from 'vite-tsconfig-paths';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
+  plugins: [react()],
   test: {
     globals: true,
     // ... Specify options here.
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
   },
 });
