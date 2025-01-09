8dfb06c14dac67a92f18ed7db0bb21a2aa7a0574
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2025-01-09 20:10:01 +0545

    docs(ui): update note about storybook with vite

--- src/ui/index.tsx
+++ src/ui/index.tsx
@@ -29,7 +29,7 @@ export function ImageContain(props: ImageProps) {
   //   />
   // );
   // NOTE Image tag adds these css position:absolute;height:100%;width:100%;left:0;top:0;right:0;bottom:0;object-fit:contain;object-position:bottom;color:transparent
-
+  // note below will not work on storybook with vite
   return (
     <Image
       style={{ objectFit: 'contain', objectPosition: 'bottom' }}
