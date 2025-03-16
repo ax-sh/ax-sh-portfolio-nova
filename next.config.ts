e26398736781f2bb2db6aae5c38c4e6df5e38146
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2025-03-16 16:04:50 +0545

    style(next.config.ts): adjust devIndicators position to top-left

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   // output: 'export', // needed for static generation
   reactStrictMode: true,
-  devIndicators: { position: 'bottom-left' },
+  devIndicators: { position: 'top-left' },
   images: { unoptimized: true },
 };
 
